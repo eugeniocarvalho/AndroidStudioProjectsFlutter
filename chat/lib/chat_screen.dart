ffa8342ed172f7ac6a11c3e52308bffff720fe3b
Eugênio Freire <eugenio9008@gmail.com>
2020-04-27 13:51:08 -0300

    Adicionado chat_message que mostra a conversa

--- chat/lib/chat_screen.dart
+++ chat/lib/chat_screen.dart
@@ -8,6 +8,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'chat_message.dart';
+
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -118,9 +120,7 @@ class _ChatScreenState extends State<ChatScreen> {
                             itemCount: documents.length,
                             reverse: true,
                             itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(documents[index].data['text']),
-                              );
+                              return ChatMessage(documents[index].data);
                             }
                         );
                     }
