9d4c902fe6c827794be0e491889145bc911350d8
Eugênio Freire <eugenio9008@gmail.com>
2020-04-28 17:00:17 -0300

    Projeto configuração inicial e funcionando

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+import 'home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,10 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter's Clothing" ,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
