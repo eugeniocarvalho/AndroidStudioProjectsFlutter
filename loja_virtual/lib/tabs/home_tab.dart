3c0db8fcad4192e245839c5623448f5bd5ca3998
Eugênio Freire <eugenio9008@gmail.com>
2020-04-28 22:01:08 -0300

    Visualização das Imagens na home

--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,22 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     //criando fundo degrade
     Widget _buildBodyBack() => Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 20, 20, 20),
-                Color.fromARGB(100, 150, 150, 150)
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter
-          )
-      ),
-    );
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 20, 20, 20),
+            Color.fromARGB(100, 150, 150, 150)
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        );
 
     //retornando stack porque quero colocar acima do fundo
     return Stack(
@@ -33,6 +30,45 @@ class HomeTab extends StatelessWidget {
                 title: const Text('Novidades'),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection('home')
+                  .orderBy('pos')
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1,
+                    crossAxisSpacing: 1,
+                    staggeredTiles: snapshot.data.documents.map(
+                        (doc){
+                          return StaggeredTile.count(doc.data['x'], doc.data['y']);
+                        }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                        (doc){
+                          return FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: doc.data['image'],
+                            fit: BoxFit.cover,
+                          );
+                        }
+                    ).toList(),
+                  );
+                }
+              },
             )
           ],
         )
