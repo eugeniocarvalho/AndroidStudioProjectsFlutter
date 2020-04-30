f4f9ed9a27cec3725c2505a213f374578f1b63c0
Eugênio Freire <eugenio9008@gmail.com>
2020-04-30 16:56:30 -0300

    Implementado a tela de Produtos no drawer

--- loja_virtual/lib/home_screen.dart
+++ loja_virtual/lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/tabs/products_tab.dart';
 import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,6 +15,14 @@ class HomeScreen extends StatelessWidget {
         Scaffold(
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Produtos'),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
         ),
         Container(color: Colors.red),
         Container(color: Colors.blue,)
