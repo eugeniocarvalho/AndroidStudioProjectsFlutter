5f8d0000ee54f20577c08da5ea1f73596b836990
Eugênio Freire <eugenio9008@gmail.com>
2020-04-30 22:08:04 -0300

    Configuração da visualização em list e em grid

--- loja_virtual/lib/tiles/product_tile.dart
+++ loja_virtual/lib/tiles/product_tile.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/datas/product_data.dart';
 
 class ProductTile extends StatelessWidget {
+
+  final ProductData data;
+  final String type;
+
+  ProductTile(this.type, this.data);
+
   @override
   Widget build(BuildContext context) {
     return Container();
