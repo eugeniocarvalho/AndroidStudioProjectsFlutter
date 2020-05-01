662f2f48864f95e5168c022d225dec80c1b97f15
Eugênio Freire <eugenio9008@gmail.com>
2020-05-01 14:20:24 -0300

    Implementação da pagina pra visualização dos produtos

--- loja_virtual/lib/tiles/product_tile.dart
+++ loja_virtual/lib/tiles/product_tile.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:lojavirtual/datas/product_data.dart';
 
 class ProductTile extends StatelessWidget {
 
-  final ProductData data;
+  final ProductData product;
   final String type;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == 'grid' ?
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 0.8,
+              child: Image.network(
+                product.images[0],
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        product.title,
+                        style: TextStyle(fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text('R\$ ${product.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
