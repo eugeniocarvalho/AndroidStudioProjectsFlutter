48386da190465ec548aa43bc6d364b543a2b4c5c
Eugênio Freire <eugenio9008@gmail.com>
2020-05-01 20:57:38 -0300

    Finalizado layout da tela de produto

--- loja_virtual/lib/screens/product_screen.dart
+++ loja_virtual/lib/screens/product_screen.dart
@@ -16,13 +16,16 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   //acessar o product daqui de dentro
   final ProductData product;
+
+  String sizeSelected;
+
   //recebendo o produto e salvando ali na linha acima
   _ProductScreenState(this.product);
+
   //serve pra nao digitar toda vez widget.prodcut.** e digitar apenas product.**
 
   @override
   Widget build(BuildContext context) {
-
     final primaryColor = Theme.of(context).primaryColor;
 
     return Scaffold(
@@ -46,16 +49,13 @@ class _ProductScreenState extends State<ProductScreen> {
             ),
           ),
           Padding(
-              padding: EdgeInsets.all(16),
+            padding: EdgeInsets.all(16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Text(
                   product.title,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                  ),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                   maxLines: 3,
                 ),
                 Text(
@@ -64,6 +64,76 @@ class _ProductScreenState extends State<ProductScreen> {
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
                       color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Tamanho',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 0.5),
+                    children: product.sizes.map((size) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            sizeSelected = size;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              border: Border.all(
+                                  color: sizeSelected == size
+                                      ? primaryColor
+                                      : Colors.grey[500],
+                                  width: 3)),
+                          width: 8,
+                          alignment: Alignment.center,
+                          child: Text(
+                            size,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    onPressed: sizeSelected != null ? () {} : null,
+                    child: Text(
+                      'Adicionar ao carrinho',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Descrição',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(
+                      fontSize: 16),
                 )
               ],
             ),
