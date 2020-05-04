cd5540d604e853243599566f834ffa77b1644753
Eugênio Freire <eugenio9008@gmail.com>
2020-05-04 18:43:18 -0300

    Alterado página para stateful, adicionado um globalkey scaffold para acessar as funções onSuccess e onFail

--- loja_virtual/lib/screens/signup_screen.dart
+++ loja_virtual/lib/screens/signup_screen.dart
@@ -1,77 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _addressController = TextEditingController();
+
 
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cadastro'),
-        centerTitle: true,
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Nome completo'),
-              validator:(text) {
-                if (text.isEmpty)
-                  return 'Nome inválido';
-                return null;
-              },
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              decoration:  InputDecoration(hintText: 'Email'),
-              validator: (text) {
-                if (text.isEmpty || !text.contains('@'))
-                  return 'E-mail inválido';
-                return null;
-              },
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Senha',
-              ),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6)
-                  return 'Senha inválida';
-                return null;
-              },
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Edenreço'),
-              validator: (text){
-                if (text.isEmpty)
-                  return 'Endereço inválido';
-                return null;
-              },
-            ),
-            SizedBox(height: 16),
-            SizedBox(
-              height: 44,
-              child: RaisedButton(
-                child: Text(
-                  'Cadastrar-se',
-                  style: TextStyle(fontSize: 18)),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: (){
-                  if(_formKey.currentState.validate()){
-                  }
-                },
-              ),
-            )
-          ],
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Cadastro'),
+          centerTitle: true,
         ),
-      ),
+        body: ScopedModelDescendant<UserModel>(
+          //tudo que estiver abaixo no scopedModelDescedant vai ter acesso ao model
+          builder: (context, child, model) {
+            if (model.isLoading)
+              return Center(child: CircularProgressIndicator());
+
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16),
+                children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(hintText: 'Nome completo'),
+                    validator: (text) {
+                      if (text.isEmpty)
+                        return 'Nome inválido';
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    validator: (text) {
+                      if (text.isEmpty || !text.contains('@'))
+                        return 'E-mail inválido';
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passController,
+                    decoration: InputDecoration(
+                      hintText: 'Senha',
+                    ),
+                    obscureText: true,
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 6)
+                        return 'Senha inválida';
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(hintText: 'Edenreço'),
+                    validator: (text) {
+                      if (text.isEmpty)
+                        return 'Endereço inválido';
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 44,
+                    child: RaisedButton(
+                      child: Text(
+                          'Cadastrar-se',
+                          style: TextStyle(fontSize: 18)),
+                      textColor: Colors.white,
+                      color: Theme
+                          .of(context)
+                          .primaryColor,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Map<String, dynamic> userData = {
+                            'name': _nameController.text,
+                            'email': _emailController.text,
+                            'address': _addressController.text
+                          };
+
+                          model.signUp(
+                              userData: userData,
+                              pass: _passController.text,
+                              onSuccess: onSuccess,
+                              onFail: onFail);
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        )
     );
   }
+
+  void onSuccess() {
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text('Usuário criado com sucesso!'),
+        backgroundColor: Theme.of(context).primaryColor,
+        duration: Duration(seconds: 1),
+      ));
+
+    Future.delayed(Duration(seconds: 2)).then((_){
+      Navigator.of(context).pop();
+    });
+  }
+
+  void onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Falha ao tentar cadastrar'),
+      backgroundColor: Colors.redAccent,
+      duration: Duration(seconds: 1),
+    ));
+  }
 }
+
