ff5cc11f2b2c7e36434d7eb3a630ba0f49415c73
Eugênio Freire <eugenio9008@gmail.com>
2020-05-04 17:14:20 -0300

    Adicionado o ScopedModel para ter acesso ao userModel

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:lojavirtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import 'screens/home_screen.dart';
 
@@ -8,14 +10,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing" ,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    //scpédmodel é para acessar o estado do usuario em qualquer lugar do app
+    return ScopedModel<UserModel>(
+        model: UserModel(),
+        //tudo que estiver abaixo no scopedModel vai ter acesso ao Usermodel e
+        // vai ser modificado, caso tenha alguma coisa aconteça no userMode
+        child: MaterialApp(
+          title: "Flutter's Clothing",
+          theme: ThemeData(
+              primarySwatch: Colors.lightBlue,
+              primaryColor: Color.fromARGB(255, 4, 125, 141)),
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
+        ));
   }
 }
