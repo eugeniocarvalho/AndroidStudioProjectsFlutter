5cc3af04d0850f901fa01b3d6f40f22e116e1089
Eugênio Freire <eugenio9008@gmail.com>
2020-05-04 17:43:32 -0300

    Implementado o código para criar um user

--- loja_virtual/lib/models/user_model.dart
+++ loja_virtual/lib/models/user_model.dart
@@ -1,12 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class UserModel extends Model{
+//usuario atual
 
-  //usuario atual
+class UserModel extends Model {
+  // isso serve pra nao ter que escrever 'FirebaseAuth.instance' o tempo todo
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //isso vai ser o usuario que estiver logado no momento, caso nao tenha é null
+  FirebaseUser firebaseUser;
+
+  //Esse map vai ter os dados mais importantes do usuario, ex: email, nome, etc
+  Map<String, dynamic> userData = Map();
+
   bool isLoading = false;
 
-  void signUp(){
+  void signUp(
+      {@required Map<String, dynamic> userData,
+      @required String pass,
+      @required VoidCallback onSucess,
+      @required VoidCallback onFail}) {
+    isLoading = true;
+    notifyListeners();
 
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData['email'], password: pass)
+        .then((user) async {
+      firebaseUser = user;
+
+      await _saveUserData(userData);
+
+      onSucess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((deuRuim) {
+      onFail();
+
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void signIn() async {
@@ -19,13 +54,17 @@ class UserModel extends Model{
     notifyListeners();
   }
 
-  void recoverPass(){
+  void recoverPass() {
 
   }
 
-  bool isLoggedIn(){
+  bool isLoggedIn() {}
 
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async{
+    //salvando os dados do usuario aqui na classe
+    this.userData = userData;
+    //salvando no firebase para que possa usar no futuro
+    await Firestore.instance.collection('users').document(firebaseUser.uid).setData(userData);
   }
-
-
 }
+
