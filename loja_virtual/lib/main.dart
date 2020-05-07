6f0e08edbf40f3c68d44598005ffd3d6c0a8c8f6
Eugênio Freire <eugenio9008@gmail.com>
2020-05-07 09:22:18 -0300

    Adicionado scopedModel do cartModel para que carrinho tenha acesso ao usuário

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/painting.dart';
 import 'package:lojavirtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
+import 'models/cart_model.dart';
 import 'screens/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -12,16 +13,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //scpédmodel é para acessar o estado do usuario em qualquer lugar do app
     return ScopedModel<UserModel>(
-        model: UserModel(),
         //tudo que estiver abaixo no scopedModel vai ter acesso ao Usermodel e
         // vai ser modificado, caso tenha alguma coisa aconteça no userMode
-        child: MaterialApp(
-          title: "Flutter's Clothing",
-          theme: ThemeData(
-              primarySwatch: Colors.lightBlue,
-              primaryColor: Color.fromARGB(255, 4, 125, 141)),
-          debugShowCheckedModeBanner: false,
-          home: HomeScreen(),
+        model: UserModel(),
+        //scoped do carrinho é abaixo do scoped do usuario porque o
+        // carrinho precisa ter acesso ao usuario atual
+        child: ScopedModel<CartModel>(
+          model: CartModel(),
+          child: MaterialApp(
+            title: "Flutter's Clothing",
+            theme: ThemeData(
+                primarySwatch: Colors.lightBlue,
+                primaryColor: Color.fromARGB(255, 4, 125, 141)),
+            debugShowCheckedModeBanner: false,
+            home: HomeScreen(),
+          ),
         ));
   }
 }
