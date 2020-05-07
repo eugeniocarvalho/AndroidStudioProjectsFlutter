1957ef1b2ea998888d1d1da25eb0206ed2ecfe4e
Eugênio Freire <eugenio9008@gmail.com>
2020-05-07 10:22:32 -0300

    Implementado as funções addCartItem e removeCartItem

--- loja_virtual/lib/models/cart_model.dart
+++ loja_virtual/lib/models/cart_model.dart
@@ -1,5 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lojavirtual/datas/cart_product.dart';
+import 'package:lojavirtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartModel extends Model {
+  //para ter o usuario atual
+  UserModel user;
 
+  //tem todos os produtos do carrinho
+  List<CartProduct> products = [];
+
+  //quando criar o cart, passa o usuario atual, para
+  // armazenar os dados no usuario atual
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct);
+
+    //salvando o item do carrinho e pegando o id do carrinho e salvando tambem
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.documentID;
+    });
+
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    Firestore.instance.collection('users').document(user.firebaseUser.uid).collection('cart').document(cartProduct.cid).delete();
+
+    products.remove(cartProduct);
+    notifyListeners();
+  }
 }
