8c8965a7d4983a63a925ed8a3036831eee6225d9
Eugênio Freire <eugenio9008@gmail.com>
2020-05-08 16:49:37 -0300

    Adicionado o card de desconto

--- loja_virtual/lib/screens/cart_screen.dart
+++ loja_virtual/lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:lojavirtual/models/cart_model.dart';
 import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/login_screen.dart';
 import 'package:lojavirtual/tiles/cart_tile.dart';
+import 'package:lojavirtual/widgets/discount_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -75,7 +76,8 @@ class CartScreen extends StatelessWidget {
                   //esta trannsformando cada um dos produtos em um cartTile que é o card
                   return CartTile(products);
                 }).toList(),
-              )
+              ),
+              DiscountCard()
             ],
           );
       }),
