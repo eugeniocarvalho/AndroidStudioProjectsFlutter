1bab41e2348d8c38e375a4c5e72c0e46e1de2a2d
Eugênio Freire <eugenio9008@gmail.com>
2020-05-20 11:41:39 -0300

    Implementado utilização do flare

--- flare_test/lib/main.dart
+++ flare_test/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _anim = 'spin';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+                if (_anim == 'spin')
+                  _anim = 'spin 2';
+                else
+                  _anim = 'spin';
+              });
+            },
+            child: FlareActor('assets/Gears.flr', animation: _anim ),
+          )
+        ),
+      ),
     );
   }
 }
